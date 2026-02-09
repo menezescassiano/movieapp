a8ab4528283f17cdabb5bf8a2feb9839940d97c8
Cassiano Menezes <cassiano.menezes@lumenalta.com>
2026-02-09 11:49:02 -0300

    Moving things around: getting rid of widgets composable file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,10 +8,9 @@ plugins {
 }
 
 android {
+
     namespace = "com.example.movieapp"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.movieapp"
